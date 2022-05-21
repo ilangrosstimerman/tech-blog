c22acd52618e1c30b347f6070f9baed5f11f8240
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-21 11:31:21 -0700

    update dashboard routes and add css

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
 const { Post } = require('../models/');
 const withAuth = require('../utils/auth');
-
 router.get('/', withAuth, async (req, res) => {
   try {
+
     const postData = await Post.findAll({
-      include: [User],
+      where:{"userId": req.session.userId}
     });
     const posts = postData.map((post) => post.get({ plain: true }));
-
     res.render('all-posts', {
       layout: 'dashboard',
       posts,
@@ -17,17 +16,14 @@ router.get('/', withAuth, async (req, res) => {
     res.redirect('login');
   }
 });
-
 router.get('/new', withAuth, (req, res) => {
   res.render('new-post', {
     layout: 'dashboard',
   });
 });
-
 router.get('/edit/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id);
-
     if (postData) {
       const post = postData.get({ plain: true });
       res.render('edit-post', {
