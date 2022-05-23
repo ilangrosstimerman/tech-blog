362d2fb5e90d06ee68e6b12c1db4ea3794d500bf
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-22 22:10:56 -0700

    try again

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: '10.0.2.2.',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     });
