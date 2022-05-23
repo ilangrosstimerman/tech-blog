e74df6a452c41cb9b069c93b1454fdac09f1f835
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-22 23:04:00 -0700

    1

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,12 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: '127.0.0.1',
-      username: 'root',
-      password: "Nate050220",
-      database: "tech_blog_db",
+      host: 'localhost.',
+      port: 3306,
       dialect: 'mysql',
-      port: 3306
+      dialectOptions: {
+        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+      }
     });
 
 module.exports = sequelize;
