c677b9b943e10f25a3834ccbe3cb6483a37bc3d7
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-23 07:21:57 -0700

    trying this

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost.',
       port: 3306,
-      dialect: 'mysql',
-      dialectOptions: {
-        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-      }
+      dialect: 'mysql'
     });
 
 module.exports = sequelize;
