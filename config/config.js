d7f7cc72a67d788a27316128b35fa379469da733
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-22 17:04:44 -0700

    try13

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ const sequelize = process.env.JAWSDB_URL
     password: "Nate050220",
     database: "tech_blog_db",
       dialect: 'mysql',
-      port: 3306
+      port: 5000
     });
 
 module.exports = sequelize;
