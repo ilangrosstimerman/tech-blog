7460e6595c62203e02467fd09fb58edc962c5dbf
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-23 07:30:59 -0700

    reinstalled node

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: '127.0.0.1.',
-      port: 4000,
+      port: 3306,
       dialect: 'mysql'
     });
 
